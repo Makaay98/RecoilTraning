b1b2f2e70d3427f099a695f0f5f306e52475d1c8
Makaay <makaay47@gmail.com>
2022-06-13 21:40:29 +0200

    init

---
+++ src/AK47Pattern.js
@@ -0,0 +1,186 @@
+export const addRecoilAK47X = (recoilX, recoilBullet) => {
+    if (recoilBullet === 1) {
+      recoilX += 5;
+    }
+    if (recoilBullet === 2) {
+      recoilX += 8;
+    }
+    if (recoilBullet === 3) {
+      recoilX -= 10;
+    }
+    if (recoilBullet === 4) {
+      recoilX += 10;
+    }
+    if (recoilBullet === 5) {
+      recoilX += 8;
+    }
+    if (recoilBullet === 6) {
+      recoilX -= 25;
+    }
+    if (recoilBullet === 7) {
+      recoilX -= 20;
+    }
+    if (recoilBullet === 8) {
+      recoilX -= 25;
+    }
+    if (recoilBullet === 9) {
+      recoilX += 25;
+    }
+    if (recoilBullet === 10) {
+      recoilX += 50;
+    }
+    if (recoilBullet === 11) {
+      recoilX += 35;
+    }
+    if (recoilBullet === 12) {
+      recoilX += 50;
+    }
+    if (recoilBullet === 13) {
+      recoilX += 9;
+    }
+    if (recoilBullet === 14) {
+      recoilX -= 48;
+    }
+    if (recoilBullet === 15) {
+      recoilX -= 48;
+    }
+    if (recoilBullet === 16) {
+      recoilX -= 32;
+    }
+    if (recoilBullet === 17) {
+      recoilX -= 48;
+    }
+    if (recoilBullet === 18) {
+      recoilX -= 17;
+    }
+    if (recoilBullet === 19) {
+      recoilX -= 25;
+    }
+    if (recoilBullet === 20) {
+      recoilX += 10;
+    }
+    if (recoilBullet === 21) {
+      recoilX += 15;
+    }
+    if (recoilBullet === 22) {
+      recoilX += 20;
+    }
+    if (recoilBullet === 23) {
+      recoilX -= 15;
+    }
+    if (recoilBullet === 24) {
+      recoilX += 18;
+    }
+    if (recoilBullet === 25) {
+      recoilX += 20;
+    }
+    if (recoilBullet === 26) {
+      recoilX += 30;
+    }
+    if (recoilBullet === 27) {
+      recoilX += 25;
+    }
+    if (recoilBullet === 28) {
+      recoilX += 25;
+    }
+    if (recoilBullet === 29) {
+      recoilX += 20;
+    }
+    if (recoilBullet === 30) {
+      recoilX += 20;
+    }
+    return recoilX;
+  };
+  export const addRecoilAK47Y = (recoilY, recoilBullet) => {
+    if (recoilBullet === 1) {
+      recoilY -= 10;
+    }
+    if (recoilBullet === 2) {
+      recoilY -= 20;
+    }
+    if (recoilBullet === 3) {
+      recoilY -= 30;
+    }
+    if (recoilBullet === 4) {
+      recoilY -= 40;
+    }
+    if (recoilBullet === 5) {
+      recoilY -= 50;
+    }
+    if (recoilBullet === 6) {
+      recoilY -= 50;
+    }
+    if (recoilBullet === 7) {
+      recoilY -= 50;
+    }
+    if (recoilBullet === 8) {
+      recoilY -= 40;
+    }
+    if (recoilBullet === 9) {
+      recoilY -= 35;
+    }
+    if (recoilBullet === 10) {
+      recoilY += 5;
+    }
+    if (recoilBullet === 11) {
+      recoilY -= 5;
+    }
+    if (recoilBullet === 12) {
+      recoilY -= 15;
+    }
+    if (recoilBullet === 13) {
+      recoilY -= 10;
+    }
+    if (recoilBullet === 14) {
+      recoilY -= 8;
+    }
+    if (recoilBullet === 15) {
+      recoilY -= 10;
+    }
+    if (recoilBullet === 16) {
+      recoilY -= 18;
+    }
+    if (recoilBullet === 17) {
+      recoilY += 10;
+    }
+    if (recoilBullet === 18) {
+      recoilY += 20;
+    }
+    if (recoilBullet === 19) {
+      recoilY -= 10;
+    }
+    if (recoilBullet === 20) {
+      recoilY -= 30;
+    }
+    if (recoilBullet === 21) {
+      recoilY += 18;
+    }
+    if (recoilBullet === 22) {
+      recoilY += 8;
+    }
+    if (recoilBullet === 23) {
+      recoilY += 28;
+    }
+    if (recoilBullet === 24) {
+      recoilY += 12;
+    }
+    if (recoilBullet === 25) {
+      recoilY -= 33;
+    }
+    if (recoilBullet === 26) {
+      recoilY += 5;
+    }
+    if (recoilBullet === 27) {
+      recoilY += 15;
+    }
+    if (recoilBullet === 28) {
+      recoilY += 15;
+    }
+    if (recoilBullet === 29) {
+      recoilY -= 8;
+    }
+    if (recoilBullet === 30) {
+      recoilY -= 8;
+    }
+    return recoilY;
+  };
--- src/App.js
+++ src/App.js
@@ -1,11 +1,97 @@
-import './App.css';
+import { useState } from "react";
+import { addRecoilAK47X, addRecoilAK47Y } from "./AK47Pattern";
+import "./App.css";
+
+let canShoot = false;
+let recoilY = 0;
+let recoilX = 0;
+let mouseX = 0;
+let mouseY = 0;
+let bullets = 30;
+let recoilBullet = 0;
+
+let rof = 100;
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  const onMouseDownHandler = (event) => {
+    //setItems([...items, { top: event.clientY, left: event.clientX }]);
+    canShoot = true;
+    let x = mouseX;
+    let y = mouseY;
+    setTimeout(() => shoot(y, x), rof);
+  };
+
+  document.onkeydown = function (evt) {
+    evt = evt || window.event;
+    var charCode = evt.keyCode || evt.which;
+    var charStr = String.fromCharCode(charCode);
+    charStr === "R" && clearItems();
+  };
+  const shoot = (y, x) => {
+    if (bullets < 1) {
+      canShoot = false;
+      return;
+    }
+    if (canShoot) {
+      setItems((prevState) => {
+        return [...prevState, { top: y + recoilY, left: x + recoilX }];
+      });
+      bullets--;
+      recoilBullet++;
+      recoilY = addRecoilAK47Y(recoilY, recoilBullet);
+      recoilX = addRecoilAK47X(recoilX, recoilBullet);
+      x = mouseX;
+      y = mouseY;
+      setTimeout(() => shoot(y, x), rof);
+    }
+  };
+  const onMouseMoveHandler = (event) => {
+    mouseX = event.clientX;
+    mouseY = event.clientY;
+  };
+  const onMouseUpHandler = (event) => {
+    canShoot = false;
+    recoilY = 0;
+    recoilX = 0;
+    recoilBullet = 0;
+  };
+  const clearItems = () => {
+    setItems([]);
+    canShoot = false;
+    recoilY = 0;
+    recoilX = 0;
+    bullets = 30;
+    recoilBullet = 0;
+  };
+
   return (
     <div className="App">
-      
+      <div
+        onMouseMove={onMouseMoveHandler}
+        onMouseDown={onMouseDownHandler}
+        onMouseUp={onMouseUpHandler}
+        className="main-div"
+      >
+        {items.map((element) => {
+          return (
+            <div
+              style={{
+                position: "fixed",
+                top: element.top,
+                left: element.left,
+                width: "10px",
+                height: "10px",
+                background: "red",
+                borderRadius: "10px",
+              }}
+            ></div>
+          );
+        })}
+      </div>
+      <button onClick={clearItems}>Reload</button>
     </div>
   );
 }
-
 export default App;
